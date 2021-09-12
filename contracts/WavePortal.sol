bc04a8f59e1faba7c0d698567655baccf3345ffb
Guido van Rossum <kc3585@nyu.edu>
2021-09-11 23:43:11 -0400

    finish local ethereum network

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,28 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint totalWaves;
+  // mapping(address => uint) public wavings;
+  // mapping (uint => address) accountIndex;
+
   constructor() {
     console.log("Yo yo, I am a contract and I am start :)!");
   }
+
+  function wave() public {
+    // wavings[msg.sender] += 1;
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+  }
+
+  function getTotalWaves() view public returns (uint) {
+    console.log("We have %d total waves", totalWaves);
+    // for(uint i=0; i < totalWaves; i++)
+    // {
+    //   console.log("%d", wavings[]);
+    // }
+    return totalWaves;
+  }
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,28 @@
 const { hexStripZeros } = require("@ethersproject/bytes");
 
 async function main() {
-  const waveContractFactory = await hre.ethers.getContractFactory(
-    "WavePortal"
-  ); // compiles the contract for us; looks in contract portal and looks for WavePortal
+  const [owner, randoPerson] = await hre.ethers.getSigners();
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // compiles the contract for us; looks in contract portal and looks for WavePortal
 
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed(); // waiting for contract to be mined
 
-  console.log("Contract deployed -- ", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+  console.log("Contract deployed to:", waveContract.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randoPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 }
 
 main()
